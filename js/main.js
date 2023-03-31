b81ea781126fc3204dde175b7bd14e75142d16d0
Juan Ezequiel Antonio <ezequiel.antonio1987@gmail.com>
2023-03-31 18:44:07 -0300

    modif estructura

--- js/main.js
+++ js/main.js
@@ -1,145 +1,44 @@
-/*const agregarNombre = (evt) => {
-  evt.preventDefault ()
-const nombre1 = document.querySelector ('#Inicio').value
-}*/
+const inputName = document.querySelector ("#input-nombre")
+const inputApellido = document.querySelector ("#input-apellido")
+const inputHuesped = document.querySelector ("#input-huesped")
+const inputFechaI = document.querySelector ("#input-fechaI")
+const inputFechaS = document.querySelector ("#input-fechaS")  
+ 
+const respName = document.querySelector ("#respNombre")
+const respApellido = document.querySelector ("#respApellido")
+const respHuesped = document.querySelector ("#respHuesped")
+const respFechaI = document.querySelector ("#respFechaI")
+const respFechaS = document.querySelector ("#respFechaS")
+
+const form = document.querySelector (".formulario")
+const respuesta = document.querySelector (".respuesta")
 
 
-const alertaForm = document.querySelector("#alerta-form");
-const alertaInput = document.querySelector("#alerta-input");
 
-alertaForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert('Bienvenid@ ' + alertaInput.value);
-  console.log (alertaInput.value)
+inputName.addEventListener("input",() => {
+    respName.innerText = inputName.value;
 });
 
-
-const nacimiento = document.querySelector("#fechaNac")
-const edad = document.querySelector("#nacimiento");
-
-nacimiento.addEventListener("submit", (e) => {
-  e.preventDefault();
-  
-  console.log(edad.value);
-
-  if (edad >= 18) {
-    alert("Podes ingresar al sitio"); console.log("Ingresa al sitio")}
-
-
-else { alert("Acceso denegado +18"); console.log("Acceso denegado") }
-  
-
+inputApellido.addEventListener("input",() => {
+    respApellido.innerText = inputApellido.value;
 });
-const edadJSON = JSON.stringify(edad);
 
-localStorage.setItem ("DatoEdad" , edadJSON);
-
-
-
-const pasajeros = document.querySelector ('#pasajeros')
-const cantidad = document.querySelector ('#cantidadPasajeros')
-
-pasajeros.addEventListener ("submit" , (e) =>{
-  e.preventDefault();
-  if (cantidad <= 2) { alert("Solicitó habitación simple"); }
-  else if (cantidad > 2 && cantidad <= 4) { alert("Solicitó habitación doble"); }
-})
-
-/*if (edad >= 18) {
-  alert("Podes ingresar al sitio"); console.log("Ingresa al sitio")
-
-
-
-  let pasajeros = parseInt(prompt("Ingrese cantidad de pasajeros:"));
-
-  while (pasajeros >= 5 || pasajeros <= 0) {
-    alert("Solo habitaciones permitidas hasta 4 personas");
-    pasajeros = parseInt(prompt("Por favor ingrese cantidad de pasajeros permitida"));
-  }
-
-  alert("Poseemos habitaciones disponibles");
-
-  if (pasajeros <= 2) { alert("Solicitó habitación simple"); }
-  else if (pasajeros > 2 && pasajeros <= 4) { alert("Solicitó habitación doble"); }
-
-  console.log(pasajeros);
-
-
-
-
-
-
-
-
-  let desayunar = prompt("Solicite Opcion de desayuno");
-
-
-  console.log(desayunar);
-
-  const desayuno = (nombre, desayunar) => {
-    console.log(nombre + " usted eligió " + desayunar + " como opción de desayuno ")
-  }
-  desayuno(nombre, desayunar);
-
-  alert("Su pedido de desayuno de " + desayunar + " se registró correctamente")
-
-  const usuario1 = {
-    nombre: nombre,
-    edad: edad,
-
-
-
-
-  }
-
-  console.log(usuario1)
-
-  const datos =
-    [nombre, edad, pais, ciudad,
-
-
-
-    ]
-
-  console.log(datos)
-
-
-  console.log(datos.length)
-  for (let i = 0; i < datos.length; i++) {
-    console.log(datos[i])
-  }
-
-  const opcion = ["te", "cafe", "mate", "tostadas", "jugo", "americano", "huevos", "medialunas", "facturas", "pan",];
-
-  if (opcion.includes(desayunar)) {
-    console.log(opcion.indexOf(desayunar));
-  } else { alert(desayunar + " no se encuentra disponible como desayuno")
-  }
-  
-}
-
-else { alert("Acceso denegado +18"); console.log("Acceso denegado") }*/
-
-
-
-//function mostrarAlerta() {alert ("Bienvenid@ " + nombre)} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+inputHuesped.addEventListener("input",() => {
+    respHuesped.innerText = inputHuesped.value;
+});
+
+inputFechaI.addEventListener("input", () => {
+    const fecha = new Date(inputFechaI.value) ;
+    respFechaI.innerHTML = fecha.toLocaleDateString();
+  });
+
+  inputFechaS.addEventListener("input", () => {
+    const fecha = new Date(inputFechaS.value) ;
+    respFechaS.innerHTML = fecha.toLocaleDateString();
+  });
+
+  form.addEventListener("submit", (evt) => {
+    evt.preventDefault ();
+   form.classList.add("confirmar");
+   respuesta.classList.add("confirmar2");
+  })
