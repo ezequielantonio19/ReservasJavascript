5772e79f285707940956e00f19e0f3f6eb090c75
Juan Ezequiel Antonio <ezequiel.antonio1987@gmail.com>
2023-04-01 22:38:17 -0300

    ciudades

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const inputName = document.querySelector ("#input-nombre")
 const inputApellido = document.querySelector ("#input-apellido")
 const inputHuesped = document.querySelector ("#input-huesped")
+const inputCiudad = document.querySelector ("#input-ciudad")
 const inputFechaI = document.querySelector ("#input-fechaI")
 const inputFechaS = document.querySelector ("#input-fechaS")  
  
 const respName = document.querySelector ("#respNombre")
 const respApellido = document.querySelector ("#respApellido")
+const respCiudad = document.querySelector ("#respCiudad")
 const respHuesped = document.querySelector ("#respHuesped")
 const respFechaI = document.querySelector ("#respFechaI")
 const respFechaS = document.querySelector ("#respFechaS")
@@ -28,6 +30,10 @@ inputHuesped.addEventListener("input",() => {
     respHuesped.innerText = "Usted solicitó ingreso para " + inputHuesped.value + " huesped/es";
 });
 
+inputCiudad.addEventListener("select",() => {
+  respCiudad.innerHTML = "En nuestro hotel Ro de la ciudad de " + inputCiudad.value ;
+});
+
 inputFechaI.addEventListener("input", () => {
     const fecha = new Date(inputFechaI.value) ;
     respFechaI.innerHTML = "Ingresando el día " + fecha.toLocaleDateString();
@@ -41,5 +47,45 @@ inputFechaI.addEventListener("input", () => {
   form.addEventListener("submit", (evt) => {
     evt.preventDefault ();
    form.classList.add("confirmar");
-   //respuesta.classList.add("confirmar2");
+   respuesta.classList.remove("confirmar");
   })
+
+
+  //6488c8e8e6e957ce5e8d887a9b7773f5
+
+  //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+  const btn = document.querySelector('#btn');
+
+  btn.addEventListener('click', ()=> {
+
+    const key = "6488c8e8e6e957ce5e8d887a9b7773f5";
+
+    let ciudad = document.querySelector('#ciudad').value;
+    ciudad = encodeURIComponent (ciudad);
+
+    if (ciudad !=""){
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}
+      `
+
+      //console.log(url)
+
+      fetch (url)
+      .then( res =>{
+        return res.json()
+      })
+
+      .then(clima =>{
+
+        console.log(clima);
+        let temp = clima.main.temp;
+        tempC = temp - 273.15;
+        let html = document.querySelector('#temperatura');
+        html.innerHTML = tempC.toFixed(0) + "°";
+
+      })
+      .catch()
+    }
+
+
+  });
